c558291fd7b4d426a2f54295562d5220d2a8d7ab
Laxman Dhulipala <laxmandhulipala@gmail.com>
2023-02-17 23:37:35 -0500

    init

--- reduce/reduce.h
+++ reduce/reduce.h
@@ -25,22 +25,3 @@ T reduce(T *A, size_t n) {
     return v1 + v2;
   }
 }
-
-//template <class T>
-//T reduce(T *A, size_t n) {
-//  if (n == 0) {
-//    return 0;
-//  } else if (n <= 1000) {
-//    T tot = 0;
-//    for (size_t i=0; i<n; ++i) {
-//      tot += A[i];
-//    }
-//    return tot;
-//  } else {
-//    T v1, v2;
-//    auto f1 = [&]() { v1 = reduce(A, n / 2); };
-//    auto f2 = [&]() { v2 = reduce(A + n / 2, n - n / 2); };
-//    par_do(f1, f2);
-//    return v1 + v2;
-//  }
-//}
